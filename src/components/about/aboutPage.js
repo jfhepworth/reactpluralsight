48f0f493dbf23bc005863b1a13c35da7141df153
Josh Hepworth <jfhepworth@gmail.com>
2018-05-02 18:30:02 +0100

    Refactored routing, added not found route

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,6 +3,20 @@
 var React = require('react');
 
 var About = React.createClass({
+	statics: {
+	willTransitionTo: function(transition, params, query, callback) {
+				if (!confirm('Are you sure you want to read a page thats this boring?')) {
+					transition.abort();
+				} else {
+					callback();
+				}
+	},	
+	willTransitionFrom: function(transition, component) {
+				if (!confirm('Are you sure you want to leave a page that\'s this interesting?')) {
+					transition.abort();
+				}
+			}
+	},
 	render: function() { 
 			return (
 				<div>
